61187f89d3fd18395c426899f61771696bac1dd8
Mandeep Sharma <mandeepvashist921@gmail.com>
2026-08-25 13:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //your JS code here. If required.
-function daysofAYear(year){
-	if(year%4==0){
+function daysOfAYear(year){
+	if((year%4==0&&year%100!==0)||year%400==0){
 		return 366;
-	}else {
-		return 365;
-	}
+	}else return 365;
 }
